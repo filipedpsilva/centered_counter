a727c0d9310434727fa754f0d3dade7a48ba2734
Filipe Silva <filipe_silva@zenbyzenith.com>
2024-08-03 21:01:51 +0000

    feat: centered button working with the right values

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { factory } from "./factory";
 
 let count = factory(0, 1);
+let factoryReset = true;
 
-function update_count_and_reset_counter() {}
+function update_count_and_reset_counter() {
+  count = factory(0, 0);
+  factoryReset = true;
+  update_text_content();
+}
 
 const start_at_control = document.getElementById(
   "start_at",
@@ -10,18 +15,30 @@ const start_at_control = document.getElementById(
 
 const step_control = document.getElementById("step") as HTMLInputElement;
 
-start_at_control?.addEventListener("change", () => {});
+start_at_control?.addEventListener("change", () => update_count_and_reset_counter());
 
-step_control?.addEventListener("change", () => {});
+step_control?.addEventListener("change", () => update_count_and_reset_counter());
 
 const count_button = document.querySelector(
   ".count_button",
 ) as HTMLButtonElement;
-
 const current_count = document.querySelector(
   ".current_count",
 ) as HTMLSpanElement;
 
-function update_count() {}
+function update_count() {
+
+  if (factoryReset) {
+    count = factory(Number(start_at_control.value), Number(step_control.value));
+    factoryReset = false;
+  }
+  
+  update_text_content();
+}
 
 count_button.addEventListener("click", update_count);
+
+function update_text_content() {
+  const value = count();
+  current_count.textContent = value.toString();
+}
